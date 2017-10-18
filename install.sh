00d4e0b2a0bae99ba12685e6739774809753a05c
Kyle <827210142@qq.com>
2017-10-18 16:12:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,8 +24,6 @@ then
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
             --with-python-config-dir=/usr/lib/python2.7/config \
-            -enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr/local
